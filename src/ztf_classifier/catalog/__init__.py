cf4b71195ab62f4e3bd9365f31758e7f097a89d0
Saeed M92 <41838398+saeed92m@users.noreply.github.com>
2026-09-25 15:33:10 +0330

    feat: add scientific catalog service

---
+++ src/ztf_classifier/catalog/__init__.py
@@ -0,0 +1,5 @@
+"""Scientific catalog query and export services."""
+
+from ztf_classifier.catalog.service import CatalogPage, ScientificCatalogService
+
+__all__ = ["CatalogPage", "ScientificCatalogService"]
